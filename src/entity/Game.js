730bd7d3096efa19e181473281bb04566415b563
Pouya Oftadeh <poftadeh2@gmail.com>
2018-08-07 05:57:11 -0400

    Fix Game module

--- src/entity/Game.js
+++ src/entity/Game.js
@@ -3,30 +3,64 @@ const Dealer = require('./Dealer.js');
 const Board = require('./Board.js');
 
 class Game {
-    constructor(players) {
-        this.players = players;
-        this.dealer = new Dealer();
-        this.board = new Board(players);
-        this.goesFirst = players[Math.floor(Math.random() * players.length)];
-        this.battle;
+    constructor(playerNames) {
+        this.playerNames = playerNames;
+        this.players = [];
         this.gameOver;
         this.numTerritoriesToWin = 4;
+        this.startNewGame();
+    }
 
+    getState() {
+        return JSON.stringify(this.players);
+    }
+
+    startNewGame() {
+        this.createPlayers();
+        this.battle;
+        this.board = new Board(this.players);
+        this.goesFirst = players[Math.floor(Math.random() * players.length)];
+        this.dealer = new Dealer();
+        this.dealer.shuffleDeck();
+        this.dealer.dealCardsToAllPlayers(7);
         this.startNewBattle();
     }
 
-    checkGameOver() {
-        this.players.forEach((player) => {
-            if(player.territories >= this.numTerritoriesToWin)
-                this.gameOver = true;
+    action(player, action, type, attackValue, swapCardType, swapCardAttackValue) {
+        if (this.battle && player === this.getPlayerTurn())
+            this.battle.turn(player, action, type, attackValue, swapCardType, swapCardAttackValue);
+        else
+            return { message: `Not ${player.name}'s turn to act!'` };
+    }
+
+    getPlayerTurn() {
+        return this.battle.playerToAct;
+    }
+
+    createPlayers() {
+        this.playerNames.forEach((name) => {
+            this.players.push(new Player(name));
         });
     }
 
+    isGameOver() {
+        let isGameOver = false;
+
+        this.players.forEach((player) => {
+            if (player.territories >= this.numTerritoriesToWin)
+                isGameOver = true;
+        });
+
+        return isGameOver;
+    }
+
     startNewBattle() {
-        if (!this.gameOver) {
+        if (!this.isGameOver()) {
+            this.battle = new Battle(this.players, this.board, this.dealer, this.goesFirst);
             this.dealer.shuffleDeck();
             this.dealer.dealCardsToAllPlayers(3);
-            this.battle = new Battle(this.players, this.board, this.dealer, this.goesFirst);
+        } else {
+            return { message: `Game is Over!` };
         }
     }
 
---
+++ tests/GameTester.js
@@ -0,0 +1,4 @@
+const Game = require('../src/entity/Game.js');
+
+const names = ['Player one','Player two']
+const game = new Game(names);
